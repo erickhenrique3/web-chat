97f9272c2715d38cf859e7dd0640b5087f64fa21
erick3 <erick.2henriquehtp@gmail.com>
2024-09-25 21:05:40 -0300

    add style home laravel

--- resources/js/Pages/Chat.vue
+++ resources/js/Pages/Chat.vue
@@ -184,7 +184,6 @@ export default {
 				Chat
 			</h2>
 		</template>
-
 		<div class="py-12">
 			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 				<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex" style="min-height: 400px; max-height: 400px">
